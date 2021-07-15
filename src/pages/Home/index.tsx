6ce83a99fbcd204b90b25a6e99432d1cc15d0953
SuccessTracker <prosdev087@gmail.com>
2021-07-14 22:18:40 -0400

    add landing page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import { Link } from "react-router-dom";
+
+const keyFrameAnim = keyframes`
+    0% {
+        background-position: top center;
+    }
+    100% {
+        background-position: bottom center;
+    }
+`;
+
+const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface IProps {
+  background: string;
+}
+
+const LeftSide = styled.div<IProps>`
+  width: 100vw;
+  height: 40vh;
+  background: url(${(p) => p.background}) no-repeat;
+  background-size: cover;
+  animation: ${keyFrameAnim} 25s ease infinite alternate;
+`;
+
+const RightSide = styled.div`
+  width: 100vw;
+  padding: 2vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StartButton = styled(Link)`
+  color: white;
+  padding: 1rem;
+  font-size: 2rem;
+`;
 
 const Home = () => {
-  return <div>Landing Page</div>;
+  return (
+    <HomeWrapper>
+      <LeftSide
+        background={
+          "https://jnswire.s3.amazonaws.com/jns-media/38/c0/11571637/personalitytest.jpeg"
+        }
+      />
+      <RightSide>
+        <StartButton
+          to="/test"
+          className="button-link"
+        >{`Start Test >> `}</StartButton>
+      </RightSide>
+    </HomeWrapper>
+  );
 };
 
 export default React.memo(Home);
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -12,6 +12,27 @@ const DarkTheme = createGlobalStyle`
     color: #FFFFFF;
     background-color: #222222;
   }
+
+  a {
+    text-decoration: none;
+  }
+
+  .button-link {
+    padding: 5px;
+    border: 1px solid rgba(255, 255, 255, 0.6);
+    border-radius: 5px;
+    cursor: pointer;
+    transition-property: opacity, transform;
+    transition-duration: 0.1s;
+    transition-timing-function: ease;
+    :hover,
+    :active {
+      transform: scale(1.1);
+        > * {
+          transform: translateY(0);
+        }
+      }
+    }
 `;
 
 export default DarkTheme;
